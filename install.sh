abc7de170cf323b8f00ef156308e1cf300c3b8f0
tam1006 <tamusyun1006@gmail.com>
2022-10-13 00:17:55 +0900

    set up for kitty

--- install.sh
+++ install.sh
@@ -3,9 +3,17 @@ download() {
     brew tap homebrew/cask-fonts
     brew install --cask font-bitstream-vera-sans-mono-nerd-font
 
+    echo "Downloading Fira Code..."
+    brew tap homebrew/cask-fonts
+    brew install --cask font-fira-code
+
     echo "Downloading starship..."
     brew install starship
     echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
+    echo "Downloading kitty..."
+    brew install --cask kitty
+    brew install --cask hammerspoon
 }
 
 main(){
