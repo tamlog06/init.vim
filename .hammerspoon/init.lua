af2547cabc6232629b60633519a21ca444546b92
tam1006 <tamusyun1006@gmail.com>
2022-10-13 00:24:19 +0900

    setup hammmerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,14 @@
+hs.hotkey.bind({"option"}, "t", function()
+    local kitty = hs.application.get("kitty")
+    if kitty == nil then
+        hs.application.launchOrFocus("/Applications/kitty.app")
+    elseif kitty:isFrontmost() then
+        kitty:hide()
+    else
+        local space = hs.spaces.focusedSpace()
+        local win = kitty:focusedWindow()
+        hs.spaces.moveWindowToSpace(win, space)
+        win:focus()
+    end
+end)
+
