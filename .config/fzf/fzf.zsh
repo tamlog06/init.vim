507c173c4a7f3957f6691438227ee17790bd7014
tamlog06 <tamusyun1006@gmail.com>
2023-02-22 14:18:08 +0900

    update to latest settings as mac

--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -1,4 +1,5 @@
 # Setup fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 # ---------
 #if [[ ! "$PATH" == */Users/rockyzhang/gitrepos/fzf/bin* ]]; then
   #PATH="${PATH:+${PATH}:}/Users/rockyzhang/gitrepos/fzf/bin"
@@ -7,9 +8,9 @@
 # Auto-completion
 # ---------------
 #[[ $- == *i* ]] && source "/Users/rockyzhang/gitrepos/fzf/shell/completion.zsh" 2> /dev/null
-source "~/dotfiles/.config/fzf/completion.zsh"
+#source "~/dotfiles/.config/fzf/completion.zsh"
 
 # Key bindings
 # ------------
 #source "/Users/rockyzhang/gitrepos/fzf/shell/key-bindings.zsh"
-source "~/dotfiles/.config/fzf/key-bindings.zsh""
+#source "~/dotfiles/.config/fzf/key-bindings.zsh""
